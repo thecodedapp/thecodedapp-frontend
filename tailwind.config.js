acbc7010fcbe333482f355a71dfa906cc9aace53
schedaura <contact@schedaura.com>
2025-08-17 11:34:08 -0400

    expo app setup

---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+/******** tailwind.config.js ********/
+module.exports = {
+    content: ["./App.{js,jsx,ts,tsx}", "./app/**/*.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"],
+    theme: { extend: {} },
+    plugins: [],
+};
