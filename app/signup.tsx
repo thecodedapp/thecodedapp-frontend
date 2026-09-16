c24387ff5c75863ad596582888ec98bb01ba52e2
thecodedapp <contact@learnmaco.com>
2026-09-16 19:04:08 -0400

    Fix signup pond background TypeScript style

--- app/signup.tsx
+++ app/signup.tsx
@@ -282,7 +282,11 @@ const styles = StyleSheet.create({
     justifyContent: "flex-end",
   },
   pondBackground: {
-    ...StyleSheet.absoluteFillObject,
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
     width: "100%",
     height: "100%",
   },
