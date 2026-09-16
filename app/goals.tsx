b924f65598a903884587a767d88e49882a46d0da
thecodedapp <contact@learnmaco.com>
2026-09-16 19:03:43 -0400

    Fix goals pond background TypeScript style

--- app/goals.tsx
+++ app/goals.tsx
@@ -342,7 +342,11 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   pondBackground: {
-    ...StyleSheet.absoluteFillObject,
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
     width: "100%",
     height: "100%",
   },
